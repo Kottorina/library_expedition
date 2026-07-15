9c8267edf256e11c3a7112b745fc408ccc3de2f3
Kottorina <sashakottorin@gmail.com>
2026-07-15 06:53:53 +0500

    0.03

--- Scene/Level generator/level_generator.gd
+++ Scene/Level generator/level_generator.gd
@@ -10,8 +10,8 @@ extends Node
 func test():
 	generate_level(curent_level)
 
-var connectors_dict : Dictionary #piece_connector
-var wall_floor_dict : Dictionary #piece_connector
+var connectors_dict : Dictionary = { } ##piece_connector
+var wall_floor_dict : Dictionary = { } ##piece_wall_floor...
 
 func clear() -> void: ##Очистка отрисовки связанной с уровнем
 	connectors_dict.clear()
@@ -35,53 +35,55 @@ func generate_level(curent_level_ : level):
 	clear()
 
 func place_corridors(curent_level_ : level,rnd_ : RandomNumberGenerator) -> void:
-	var start_corridors_connectors : Array[piece_connector] = []
+	
+	var local_connectors_dict : Dictionary ##Нужен при локальной генерации
+	
 	for i in connectors_dict:
 		var connector : piece_connector = connectors_dict[i]
 		if connector.type == 1:
-			start_corridors_connectors.append(connector)
-	
-	var corridors_connectors : Array[piece_connector] = []
-	corridors_connectors = start_corridors_connectors
+			local_connectors_dict[connector.coord_] = connector
 	
 	for step_global in curent_level_.corridor_step_generate:
 		
-		var new_st_cor_con : Array[piece_connector] = []
+		var new_step_connectors_dict : Dictionary = {}
 		
-		for connector in corridors_connectors:
+		for c_key in local_connectors_dict:
+			var connector : piece_connector = local_connectors_dict[c_key]
 			
 			var direction = connector.direction
-			var coord_connector = connector.coord_
+			var coord_connector = c_key
 			
 			var min_max = curent_level_.corridor_min_max_long
 			var number_piece = rnd_.randi_range(min_max.x,min_max.y)
 			for step_local in number_piece:
 				var piece_connectors = get_piece_connectors_from_set("base",direction,curent_level_.corridors_piece_set)
 				
-				var coord_new_connector = DIRECTION_VECTOR[direction] + coord_connector #Относительно крайнего коннектора  
+				var coord_new_connector = DIRECTION_VECTOR[direction] + coord_connector #Относительно крайнего коннектора
 				var offset = coord_new_connector - piece_connectors[1].coord_ #Относительно нуля смешение
+				
 				place_piece(offset, piece_connectors[0])
-				
-				if step_global == 1:
-					print(offset)
-				
-				coord_connector = ((piece_connectors[1].coord_ + piece_connectors[2][0].coord_) + coord_new_connector)
-				
+				coord_connector = ((piece_connectors[2][0].coord_-piece_connectors[1].coord_) + coord_new_connector)
+
 				if step_local+1 == number_piece:
 					
 					if step_global +1 != curent_level_.corridor_step_generate:
 						
 						piece_connectors = get_piece_connectors_from_set("fork",direction,curent_level_.corridors_piece_set)
+				#
+						coord_new_connector = DIRECTION_VECTOR[direction] + coord_connector #Относительно крайнего коннектора
+						offset = coord_new_connector - piece_connectors[1].coord_ #Относительно нуля смешение
+						place_piece(offset, piece_connectors[0])
 						
-						coord_new_connector = DIRECTION_VECTOR[direction] + coord_connector
-						offset = piece_connectors[1].coord_ - coord_new_connector
-						place_piece(-offset, piece_connectors[0])
-						
-						coord_connector = ((piece_connectors[1].coord_ + piece_connectors[2][0].coord_) + coord_new_connector)
-						
-						new_st_cor_con.append_array(piece_connectors[2])
-		
-		corridors_connectors = new_st_cor_con
+						for p_c : piece_connector in piece_connectors[2]:
+							coord_connector = ((p_c.coord_-piece_connectors[1].coord_) + coord_new_connector)
+							new_step_connectors_dict[coord_connector] = p_c
+							
+		local_connectors_dict.clear()
+		local_connectors_dict.merge(new_step_connectors_dict)
+							
+		#				new_st_cor_con.append_array(piece_connectors[2])
+		#return
+		#corridors_connectors = new_st_cor_con
 
 
 const DIRECTION_VECTOR = {
@@ -99,6 +101,7 @@ func get_piece_connectors_from_set(type : String,direction : String,corridor_set
 					for connector in piece_.connectors_level:
 						if connector.direction == direction:
 							
+							var exit_connector : piece_connector
 							var oth_connectors = piece_.connectors_level.duplicate()
 							oth_connectors.erase(connector)
 							
@@ -114,7 +117,7 @@ func get_piece_connectors_from_set(type : String,direction : String,corridor_set
 							return [piece_,connector,oth_connectors]
 
 func place_piece(coord_piece : Vector2i, piece : level_piece):
-	for i : piece_connector in piece.connectors_level:
+	for i : piece_connector in piece.connectors_level: ##ТУТ ВСЕ ПЛОХО, ПОДУМАЙ
 		connectors_dict[i.coord_ + coord_piece] = i
 	for i : piece_wall_floor in piece.wall_floor_level:
 		wall_floor_dict[i.coord_ + coord_piece] = i

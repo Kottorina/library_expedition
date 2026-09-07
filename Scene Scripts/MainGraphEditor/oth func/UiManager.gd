ccae67633c324666de3196b1ddf1119133b5fc10
Kottorina <sashakottorin@gmail.com>
2026-09-07 12:00:08 +0500

    0.13.1.1

--- Scene Scripts/MainGraphEditor/oth func/UiManager.gd
+++ Scene Scripts/MainGraphEditor/oth func/UiManager.gd
@@ -1,40 +1,27 @@
 extends Node
 
 @export var main_graph_editor : Node
-
 @export var graph_node_ui : Control
 
-const TOOLTITLE : String = "Tool:"
-const TOOLTYPE : int = 5 ##Для передачи технических значений
-const TOOLCOLOR :=  Color.WHITE
+var ui_const_func := UiConstFunc.new()
 
-## КАТЕГОРИЯ ГРУППЫ
-const TOOL_FORK_UI_NAME : String = "Tool Fork"
-const TOOL_UI_NAME : String = "Tool"
-const ROOMS_UI_NAME : String = "Rooms"
-const DIALOGUE_UI_NAME : String = "Dialogue"
+## ДЛЯ BIG_INSTR
+const ROOM_BIG_TITLE = "Room Node: "
+const ENTER_LOCATION_BIG_TITLE : String = "Enter Location Node"
+const START_GENER_LOCATION_BIG_TITLE : String = "Start Gener Node"
+const BIG_RND_FORK_BIG_TITLE : String = "Rnd Fork"
 
-const ENTER_LOCATION_TITLE : String = "Id Enter:"
-const ROOMNODENAME = "Room Node: "
-const ENTER_LOCATION_NAME : String = "Enter Location Node"
-const START_GENER_LOCATION_TITLE : String = "Start Gener Node"
+const CONNECTOR_TYPE_COUNT = 3 ## WHITE GRAY BLACK
 
-const BIG_RND_FORK_TITLE : String = "Rnd Fork"
-const INSTR_FORK_BASE_TITLE : String = "Base Option: "
-const INSTR_FORK_ALT_CHANCE_TITLE : String = "Alt Chance: "
-
-const CONNECTOR_TYPE_COUNT = 3
-const CONNECTOR_DIRECTION_COUNT = 4
 const CONNECTOR_SIZE_COUNT = 3
+const SIZE : Array[String] = ["tiny","small","medium","large"]
 
 var toolconnectordirection_ar : Array[Array] = [[true,false],[false,true],[true,false],[false,true]]
 const DIRECTION : Array[String] = ["up","down","left","right"] 
 const DIRECTION_INVERT : Array[int] = [1,0,3,2] 
 
 const BASECOLORTYPE : Array[String] = ["black","gray","white"]
-const SIZE : Array[String] = ["tiny","small","medium","large"]
 
-const TOOLCONNECTORTITLE : String = "Connector:"
 const CONNECTORTYPEDICT : Dictionary = {
 0 : [5,6,7,8],
 1 : [9,10,11,12],
@@ -45,9 +32,7 @@ var CONNECTORCOLORDICT : Dictionary = {
 1 : [Color.LIGHT_GOLDENROD,Color.YELLOW,Color.DARK_KHAKI],
 2 : [Color.LIGHT_CORAL,Color.RED,Color.DARK_RED]
 }
-const TOOLENTERTITLE : String = "Enter:"
-const TOOLENTERTYPE : int = 17
-const TOOLENTERCOLOR : Color = Color.PURPLE
+
 
 var room_set : RoomsSet
 #var ready_location_ar : Array[ReadyLocation]
@@ -121,34 +106,22 @@ func bake_ui_rooms_nodes() -> void:
 		var current_room = room_set.rooms_ar[room_ind]
 		
 		if current_room != null:
-			var name_item : String = ROOMNODENAME + current_room.name_+" "+str(room_ind)
+			var name_item : String = ROOM_BIG_TITLE + current_room.name_+" "+str(room_ind)
 			
 			var big_instr = room_to_biginstrgraphnode(current_room)
 			big_instr.title_node = name_item
 			big_instr.room_ = current_room
 			
-			graph_node_ui.AddNewUiItem(ROOMS_UI_NAME, big_instr)
+			graph_node_ui.AddNewUiItem(ui_const_func.ROOMS_UI_NAME, big_instr)
 
 func bake_ui_enter_node() -> void: ##Универсальный нод для обозначения входа на локу
 	var big_instr = BigGraphNodeMakeInsts.new()
-	big_instr.title_node = ENTER_LOCATION_NAME
+	big_instr.title_node = ENTER_LOCATION_BIG_TITLE
 	big_instr.type_node = 1
 	
-	var instr = GraphNodeMakeInsts.new()
-	instr.body_node = 1
-	instr.title_instr = ENTER_LOCATION_TITLE
+	big_instr.instr_ar.append(ui_const_func.get_tool_enter_instr())
 	
-	
-	instr.is_right = true 
-	instr.right_type = TOOLENTERTYPE
-	instr.right_color = TOOLENTERCOLOR
-	instr.is_left = true
-	instr.left_type = TOOLENTERTYPE
-	instr.left_color = TOOLENTERCOLOR
-	
-	big_instr.instr_ar.append(instr)
-	
-	graph_node_ui.AddNewUiItem(TOOL_UI_NAME, big_instr)
+	graph_node_ui.AddNewUiItem(ui_const_func.TOOL_UI_NAME, big_instr)
 
 const CONNECTORS_PLUGS_TITLE : String = "Connectors Plugs Node"
 func bake_ui_connectors_plugs() -> void:
@@ -156,26 +129,18 @@ func bake_ui_connectors_plugs() -> void:
 	big_instr.title_node = CONNECTORS_PLUGS_TITLE
 	big_instr.type_node = 4
 	
-	var tool_instr = GraphNodeMakeInsts.new()
-	tool_instr.title_instr = TOOLTITLE
-	tool_instr.is_right = true
-	tool_instr.right_type = TOOLTYPE
-	tool_instr.right_color = TOOLCOLOR
-	tool_instr.is_left = true
-	tool_instr.left_type = TOOLTYPE
-	tool_instr.left_color = TOOLCOLOR
-	big_instr.instr_ar.append(tool_instr)
+	big_instr.instr_ar.append(ui_const_func.get_tool_instr())
 	
 	for size_ in CONNECTOR_SIZE_COUNT:
 		for type_ in CONNECTOR_TYPE_COUNT:
-			for direction_ in CONNECTOR_DIRECTION_COUNT:
+			for direction_ in DIRECTION.size():
 				var connector := RoomConnector.new()
 				connector.type = type_
 				connector.direction = direction_
 				connector.size_ = size_
 				big_instr.instr_ar.append(make_inst_from_connector(connector))
 
-	graph_node_ui.AddNewUiItem(TOOL_UI_NAME, big_instr)
+	graph_node_ui.AddNewUiItem(ui_const_func.TOOL_UI_NAME, big_instr)
 
 ## А НАХУЙ, ПОДУМАЙ, БЛЯДЬ (это обращение)
 func bake_custom_big_instr() -> void:
@@ -186,30 +151,19 @@ func bake_custom_big_instr() -> void:
 
 func bake_ui_start_gener_node() -> void: ##Стартовая хуйня, без нее генерация по пизде идет
 	var big_instr = BigGraphNodeMakeInsts.new()
-	big_instr.title_node = START_GENER_LOCATION_TITLE
+	big_instr.title_node = START_GENER_LOCATION_BIG_TITLE
 	big_instr.type_node = 2
 	
-	var tool_instr = GraphNodeMakeInsts.new()
-	tool_instr.title_instr = TOOLTITLE
-	tool_instr.is_right = true
-	tool_instr.right_type = TOOLTYPE
-	tool_instr.right_color = TOOLCOLOR
-	big_instr.instr_ar.append(tool_instr)
+	big_instr.instr_ar.append(ui_const_func.get_tool_instr())
 	
-	var enter_instr = GraphNodeMakeInsts.new()
-	enter_instr.is_right = true 
-	enter_instr.body_node = 1
-	enter_instr.title_instr = ENTER_LOCATION_TITLE
-	enter_instr.right_type = TOOLENTERTYPE
-	enter_instr.right_color = TOOLENTERCOLOR
-	big_instr.instr_ar.append(enter_instr)
+	big_instr.instr_ar.append(ui_const_func.get_tool_enter_instr())
 	
-	graph_node_ui.AddNewUiItem(TOOL_UI_NAME, big_instr)
+	graph_node_ui.AddNewUiItem(ui_const_func.TOOL_UI_NAME, big_instr)
 	
 func bake_ui_all_rnd_fork() -> void: ## Случайные Перекрестки, Очень Круто
 	for type_ in CONNECTOR_TYPE_COUNT:
 		for size_ in CONNECTOR_SIZE_COUNT:
-			for direction_ in CONNECTOR_DIRECTION_COUNT:
+			for direction_ in DIRECTION.size():
 				var connector := RoomConnector.new()
 				connector.type = type_
 				connector.direction = direction_
@@ -220,35 +174,27 @@ func bake_ui_rnd_fork_from_connector( enter_connector : RoomConnector ) -> void:
 	var big_instr = BigGraphNodeMakeInsts.new()
 	big_instr.type_node = 5
 	
-	var tool_instr = GraphNodeMakeInsts.new()
-	tool_instr.title_instr = TOOLTITLE
-	tool_instr.is_right = true
-	tool_instr.right_type = TOOLTYPE
-	tool_instr.right_color = TOOLCOLOR
-	tool_instr.is_left = true
-	tool_instr.left_type = TOOLTYPE
-	tool_instr.left_color = TOOLCOLOR
-	big_instr.instr_ar.append(tool_instr)
+	big_instr.instr_ar.append(ui_const_func.get_tool_instr())
 	
 	var enter_instr = make_inst_from_connector(enter_connector)
 	big_instr.instr_ar.append(enter_instr)
 	
-	var big_title = BIG_RND_FORK_TITLE + " " + enter_instr.title_instr
+	var big_title = BIG_RND_FORK_BIG_TITLE + " " + enter_instr.title_instr
 	
 	big_instr.title_node = big_title
 	
 	var exit_connector = enter_connector.duplicate()
 	exit_connector.direction = DIRECTION_INVERT[enter_connector.direction]
 	var exit_instr_one = make_inst_from_connector(exit_connector)
-	exit_instr_one.title_instr = INSTR_FORK_BASE_TITLE
+	exit_instr_one.title_instr = ui_const_func.INSTR_FORK_BASE_TITLE
 	big_instr.instr_ar.append(exit_instr_one)
 	
 	var exit_instr_two = make_inst_from_connector(exit_connector)
 	exit_instr_two.body_node = 1
-	exit_instr_two.title_instr = INSTR_FORK_ALT_CHANCE_TITLE
+	exit_instr_two.title_instr = ui_const_func.INSTR_FORK_ALT_CHANCE_TITLE
 	big_instr.instr_ar.append(exit_instr_two)
 	
-	graph_node_ui.AddNewUiItem(TOOL_FORK_UI_NAME, big_instr)
+	graph_node_ui.AddNewUiItem(ui_const_func.TOOL_FORK_UI_NAME, big_instr)
 
 func room_to_biginstrgraphnode(room : Room) -> BigGraphNodeMakeInsts:
 	var big_instr = BigGraphNodeMakeInsts.new()
@@ -258,18 +204,11 @@ func room_to_biginstrgraphnode(room : Room) -> BigGraphNodeMakeInsts:
 
 		big_instr.instr_ar.append(make_inst_from_connector(connector))
 	for enter : RoomEnter in room.room_enter_ar:
-		var instr = GraphNodeMakeInsts.new()
+		
+		var instr = ui_const_func.get_tool_enter_instr()
 		instr.body_node = 0
 		instr.source_res = enter
-		instr.title_instr = " ".join([TOOLENTERTITLE, BASECOLORTYPE[enter.type]]) 
-		
-		instr.is_left = true
-		instr.left_type = TOOLENTERTYPE
-		instr.left_color = TOOLENTERCOLOR
-		instr.is_right = true
-		instr.right_type = TOOLENTERTYPE
-		instr.right_color = TOOLENTERCOLOR
-		
+		instr.title_instr = " ".join([ui_const_func.TOOLENTERTITLE, BASECOLORTYPE[enter.type]]) 
 		big_instr.instr_ar.append(instr)
 	
 	return big_instr
--- addons/libraryeditor/Custom Class/GraphVisualData/Ui/ui_const.gd
+++ addons/libraryeditor/Custom Class/GraphVisualData/Ui/ui_const.gd
@@ -1,2 +1,75 @@
 extends Resource
-class_name UiConst
+class_name UiConstFunc
+
+## Типы коннекторов
+const TOOLTITLE : String = "Tool:"
+const TOOLTYPE : int = 5 ##Для передачи технических значений
+const TOOLCOLOR :=  Color.WHITE
+
+const TOOLENTERTITLE : String = "Enter:"
+const TOOLENTERTYPE : int = 17
+const TOOLENTERCOLOR : Color = Color.PURPLE
+
+## КАТЕГОРИЯ ГРУППЫ
+const TOOL_FORK_UI_NAME : String = "Tool Fork"
+const TOOL_UI_NAME : String = "Tool"
+const ROOMS_UI_NAME : String = "Rooms"
+const DIALOGUE_UI_NAME : String = "Dialogue"
+
+## ДЛЯ INSTR
+const ENTER_LOCATION_TITLE : String = "Id Enter:"
+const INSTR_FORK_BASE_TITLE : String = "Base Option: "
+const INSTR_FORK_ALT_CHANCE_TITLE : String = "Alt Chance: "
+const TOOL_CONNECTOR_TITLE : String = "Connector:"
+
+## ОСТАЛЬНЫЕ КОНСТАНТЫ
+const DIRECTION : Array[String] = ["up","down","left","right"] 
+const SIZE : Array[String] = ["tiny","small","medium","large"]
+
+## МИНИ ИНСТРУКЦИЯ ДЛЯ TOOL
+func get_tool_instr() -> GraphNodeMakeInsts:
+	
+	var tool_instr = GraphNodeMakeInsts.new()
+	tool_instr.title_instr = TOOLTITLE
+	
+	tool_instr.is_right = true
+	tool_instr.right_type = TOOLTYPE
+	tool_instr.right_color = TOOLCOLOR
+	tool_instr.is_left = true
+	tool_instr.left_type = TOOLTYPE
+	tool_instr.left_color = TOOLCOLOR
+	
+	return tool_instr
+
+## МИНИ ИНСТРУКЦИЯ ДЛЯ ENTER
+func get_tool_enter_instr() -> GraphNodeMakeInsts:
+	var instr = GraphNodeMakeInsts.new()
+	instr.body_node = 1
+	instr.title_instr = ENTER_LOCATION_TITLE
+	
+	
+	instr.is_right = true 
+	instr.right_type = TOOLENTERTYPE
+	instr.right_color = TOOLENTERCOLOR
+	instr.is_left = true
+	instr.left_type = TOOLENTERTYPE
+	instr.left_color = TOOLENTERCOLOR
+	
+	return instr
+
+## ИНСТРУКЦИЯ ИЗ КОННЕКТОРА
+func make_inst_from_connector(connector : RoomConnector ) -> GraphNodeMakeInsts:
+	var instr := GraphNodeMakeInsts.new()
+
+	instr.body_node = 0
+	instr.source_res = connector
+	instr.title_instr = " ".join([TOOL_CONNECTOR_TITLE, DIRECTION[connector.direction], BASECOLORTYPE[connector.type], SIZE[connector.size_]])
+	
+	instr.is_left = toolconnectordirection_ar[connector.direction][0]
+	instr.is_right = toolconnectordirection_ar[connector.direction][1]
+	instr.left_type = CONNECTORTYPEDICT[connector.type][connector.size_]
+	instr.right_type = CONNECTORTYPEDICT[connector.type][connector.size_]
+	instr.left_color = CONNECTORCOLORDICT[connector.type][connector.size_]
+	instr.right_color = CONNECTORCOLORDICT[connector.type][connector.size_]
+	
+	return instr
